7b94b2579e9ce78a84098503ca8c42461bbdf992
ujjwaladhikari <70739195+ujjwaladhikari@users.noreply.github.com>
2020-09-30 12:45:12 +0530

    robofriends project

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Mahesh Adhikari',
+	username: 'mahi',
+	email: 'maheshadhikari@gmail.com'
+},
+{
+	id: 2,
+	name: 'Sourav Adhikari',
+	username: 'Loku',
+	email: 'souravadhikari@gmail.com'
+},
+{
+	id: 3,
+	name: 'Badal Chowdhary',
+	username: 'Panda',
+	email: 'badalchowdhary@gmail.com'
+},
+{
+	id: 4,
+	name: 'Ashish Adhikari',
+	username: 'Jack',
+	email: 'ashishadhikari@gmail.com'
+},
+{
+	id: 5,
+	name: 'Ayush Tamang',
+	username: 'sus',
+	email: 'ayushtamang@gmail.com'
+},
+{
+	id: 6,
+	name: 'Samir Mamba',
+	username: 'bhanu',
+	email: 'bhanumamba@gmail.com'
+},
+{
+	id: 7,
+	name: 'Ajit Pagla',
+	username: 'jakob',
+	email: 'paglajackob@gmail.com'
+},
+{
+	id: 8,
+	name: 'Ujjal Adhikari',
+	username: 'ujjal',
+	email: 'ujjaladhikari@gmail.com'
+},
+{
+	id: 9,
+	name: 'Someone Pdhikari',
+	username: 'one',
+	email: 'forsomething@gmail.com'
+},
+{
+	id: 10,
+	name: 'Lovely Sharma',
+	username: 'love',
+	email: 'loveiseverywhere@gmail.com'
+}
+]
